25e7c87e525a1f54501e143720c74ffafbfe03f6
Rhythunand <rhythunandbiju@gmail.com>
2025-01-18 14:50:49 +0300

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,10 @@
+def fibonacci(n) :
+  n = int(input("Enter a number : "))
+  temp = n
+  a, b = 0, 1
+  count = 0
+  while count < n :
+    print(a , end = ' ')
+    a, b = b, a + b
+    count += 1
+fibonacci(10)
